7472e434a9e1a6d85610d03295e509016059a9a2
zhonghaisheng <a13202564692@163.com>
2017-09-26 15:18:57 +0800

    change

--- webpack.config.js
+++ webpack.config.js
@@ -36,10 +36,11 @@ module.exports = merge(bundleWebpack, {
     plugins: [
         //不需要打包html将此处注释便可
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.DllReferencePlugin({
-            context: __dirname,
-            manifest: require('./vendor/vendor-manifest.json')
-        }),
+        //打包时开启，可加快打包速度，开发时要关闭
+        // new webpack.DllReferencePlugin({
+        //     context: __dirname,
+        //     manifest: require('./vendor/vendor-manifest.json')
+        // }),
         //webpack定义全局变量
         new webpack.ProvidePlugin({
             $: "jquery",

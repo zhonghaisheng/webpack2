6663e57aff199ac34719f5b898a014e463cf3c04
zhonghaisheng <a13202564692@163.com>
2017-09-26 15:07:46 +0800

    change

--- webpack.config.dll.js
+++ webpack.config.dll.js
@@ -1,20 +1,21 @@
-const path    = require('path');
+//dll打包配置文件（一次配置，永久使用，不参与打包中）
+const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: {
-      vendor: ['vue','jquery']
-  },
-  output: {
-    path: path.join(__dirname, './vendor'),
-    filename: '[name].dll.js',
-    library: '[name]_library'
-  },
-  plugins: [
-    new webpack.DllPlugin({
-      path: path.join(__dirname, './vendor', '[name]-manifest.json'),
-      name: '[name]_library',
-      context: __dirname
-    })
-  ]
+    entry: {
+        vendor: ['vue', 'jquery']
+    },
+    output: {
+        path: path.join(__dirname, './vendor'),
+        filename: '[name].dll.js',
+        library: '[name]_library'
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            path: path.join(__dirname, './vendor', '[name]-manifest.json'),
+            name: '[name]_library',
+            context: __dirname
+        })
+    ]
 };
